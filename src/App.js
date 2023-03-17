741a2077eabc35e84ffde343ce90bae9422d399b
oguzhanascier <oguzhanascier20@gmail.com>
2023-03-17 19:09:39 +0300

    import route&routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
- 
+    <div className="App"></div>
+
     </div>
   );
 }
