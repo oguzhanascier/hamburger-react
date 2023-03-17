c51f3d90c5f99daf64cba637633b0b382715bac9
oguzhanascier <oguzhanascier20@gmail.com>
2023-03-17 21:10:35 +0300

    menu component

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import { Data } from '../helpers/Data'
+import MenuItem from './MenuItem'
+import '../styles/Menu.css'
+
+
 
 function Menu() {
   return (
-    <div>Menu</div>
+    <div className='menu'>
+      <h1 className='menuTitle'>Burgers</h1>
+      <div className='menuList'>
+        {Data.map((burger, i) => {
+          return(
+            <MenuItem
+            key={i} //item key
+            image={burger.image}
+            name={burger.name}
+            content={burger.content}
+            price={burger.price}
+          />
+          )
+        })}
+      </div>
+    </div>
   )
 }
 
