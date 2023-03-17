f1284905bbe7ac1e74f8577d703bea762b96b0af
oguzhanascier <oguzhanascier20@gmail.com>
2023-03-17 20:14:39 +0300

    footer style and add social media icons

---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import '../styles/Footer.css'
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <div className='socialMedia'>
+      <FacebookIcon/>
+      <InstagramIcon/>
+      <TwitterIcon/>
+
+
+
+      </div>
+      <p>© 2023 Hamburger, Inc. </p>
+
+    </div>
+  )
+}
+
+export default Footer
